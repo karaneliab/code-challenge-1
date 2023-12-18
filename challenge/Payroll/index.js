9287ae2c8ba0692450229650a8fbbffded195ce7
karaneliab <eliabkaran7884@gmail.com>
2023-12-18 08:30:07 +0300

    added the user input that promts th user to enter the salary and the benefits

--- challenge/Payroll/index.js
+++ challenge/Payroll/index.js
@@ -1,6 +1,8 @@
-function calculatePayeeTax(salary){
+function calculatePayeTax(salary){
+    // it enables strictmode that enables detection of errors, Improved perfomance etc 
     "use strict";
-
+// declared a variable in an array method to enter the monthly salaries and their percentage 
+// deduction rates
     const taxrates = [
         [0,24000,0.1],
         [24001,32333,0.25],
@@ -8,36 +10,48 @@ function calculatePayeeTax(salary){
         [500001,800000,0.325],
         [800000,0.35]
     ];
-  
+//   initialzes the tax variable to 0
         let tax = 0;
         for (const[lower,upper,rate]of taxrates){
+            // checks if the salry falls in the above tax bracket
             if(salary <= upper){
+                // calculates using the given rate and breakout  the loop
                 tax = salary * rate;
                 break;
             }
         }
-
+// return the tax calculated
         return tax;
 
 }
-function calculateNHIF(salary){
+// to calculate NHIF based on salary
+function calculateNHIF(salary){         
+    // calculates the NHIF deduction with a five percent rate
     return salary * 0.05; 
 }
+// to calculate nssf based on salary
 function calculateNSSF(salary){
+    // calculates the nssf deduction with a six percent rate
     return salary * 0.06;
 }
-function calculateNetSalary(salary,payeeTax,nhif,nssf){
-    return salary - payeeTax - nhif -nssf;
+// to calculate using salary,payeetax,nhif and nssf
+function calculateNetSalary(salary,PayeTax,nhif,nssf){
+    return salary - PayeTax - nhif -nssf;
 }
 function main(){
+    // sample of salary and benefits
     const salary = 15000;
     const benefits = 5000;
-    constPayeeTax = calculatePayeeTax(salary + benefits);
+    // calculate Payee tax by adding salary and benefits
+    const PayeTax = calculatePayeTax(salary + benefits);
+    // calculates NHIF based on the salary
     const nhif = calculateNHIF(salary);
+    // calculates NSSF based on salary
     const nssf = calculateNSSF(salary);
-    const netSalary = calculateNetSalary(salary + benefits + payeeTax + nhif + nssf);
-
-        console.log("Payee Tax:",payeeTax);
+    // calculates netSalary base 0n all the above
+    const netSalary = calculateNetSalary(salary + benefits + PayeTax + nhif + nssf);
+// logs out the above calculated values
+        console.log("Payee Tax:",PayeTax);
         console.log("NHIF Deduction:", nhif);
         console.log("NSSF Deductions:",nssf);
         console.log("Net Salary:",netSalary);
@@ -45,3 +59,13 @@ function main(){
 
 }
 main();
+
+// Added userInput that prompts the user to enter the Salary and the benefits when runned in liveserver.
+function getUserSalaryandBenefits (){
+    const salary = parseFloat(prompt("Enter your Salary:"));
+    const benefits = parseFloat(prompt("Enter your Benefits:"));
+    return {salary,benefits};
+}
+const userInput = getUserSalaryandBenefits();
+const {salary, benefits} = userInput;
+main(salary, benefits);
---
+++ challenge/README.md
@@ -0,0 +1,15 @@
+## AUTHOR NAME:
+Eliab Kipleting Karan
+
+## Running Instructions
+Ensure node is installed
+To run the command use, node filename.js
+
+### More About
+This is a js program only it has three main files. It contain one for grading,payroll-like and Speed detector
+
+### Grading
+In this the user enters the student score through a prompt user input and the user will get the output of it in the console after inspection of the page.
+
+### Payroll-like
+This one is used to calculate various things ie Payee, NNSF,NHIF and the Net Salary of the user.The user inputs the  salary and the the Benefits and he/she will be ableto calculate the the deductions and the netsalaries.  
